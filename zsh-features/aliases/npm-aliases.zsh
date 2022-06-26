cc0cdb5700c20fa87ada3e5125f8ad7632400464
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2022-06-26 23:48:43 +0200

    sh: fixes npm alias for fish

--- zsh-features/aliases/npm-aliases.zsh
+++ zsh-features/aliases/npm-aliases.zsh
@@ -2,7 +2,7 @@
 alias n='npm'
 alias ni='npm install'
 alias nr='npm run'
-alias nrs='echo "$(tab_color_yellow)" && npm run start'
+alias nrs='echo "$tab_color_yellow" && npm run start'
 alias npmu='npm update'
 alias npmr='npm run'
 
