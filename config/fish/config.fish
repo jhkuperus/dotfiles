122e63e16b9dbf173bce2f780799a3b89195ad86
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2022-06-27 20:54:45 +0200

    fish: adds PATH and Homebrew shellenv to fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set PATH /usr/local/bin:$PATH
+
+set HOMEBREW /opt/homebrew/bin/brew
+if test -f $HOMEBREW 
+  eval $($HOMEBREW shellenv)
+end
+
 starship init fish | source
 
 # Load aliases from dotfiles
